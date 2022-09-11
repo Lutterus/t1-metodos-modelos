48d50cb373bc8fa6801089ca4a545a404df954e1
Joao Bitelo <joao.bitelo@quickdata.com.br>
2022-09-11 19:00:55 -0300

    cria descrição Gherkin com cenarios de teste

---
+++ src/test/java/bee1158/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package bee1158;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("bee1158")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
